085b804e7fb595984195b50d1c9777fe71d523b8
Jake Furtaw <120673183+JakeFurtaw@users.noreply.github.com>
2024-04-24 19:17:44 -0400

    Add files via upload

---
+++ HTMLParser.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+
+# Provide the path to your local HTML file
+file_path = "/home/jake/Programming/LangchainRAG-Chatbot/parsers/HTMLtoText/Jal Irani _ Towson University.html"
+
+# Open the file and read its content
+with open(file_path, "r", encoding="utf-8") as file:
+    html_content = file.read()
+
+# Parse the HTML content using Beautiful Soup
+soup = BeautifulSoup(html_content, "html.parser")
+
+# Remove the content within div elements with specific class names
+for div in soup.select('div#skip-to-main, div.row, div.utility, div.main, div.mobile, div.links, div.secondary, div.bottom'):
+    div.decompose()
+
+# Extract the text content while removing tags, scripts, and styles
+text_only = soup.get_text(strip=True, separator="\n")
+
+print(text_only)
